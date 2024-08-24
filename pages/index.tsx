f454355e7be544152ab072992887218ee728c393
Azka190 <azkarehman2001@gmail.com>
2024-08-24 10:02:57 +0500

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import axios from "axios";
+import { error } from "console";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const newQuote = () => {
+    axios.get('https://api.quotable.io/random').then(response =>{
+      console.log(response)
+    }).catch(error =>{
+      console.log(error)
+    } )
+  }
   return (
    <>
+   <button onClick={newQuote} className="py-2 px-4 border">Display</button>
    </>
   );
 }
